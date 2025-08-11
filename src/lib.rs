671b051b4c442fc31de4373704822bb96bf45709
jadahbakar <dedy.styawan@gmail.com>
2025-08-11 14:40:17 +0700

    config

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
+use std::error::Error;
+
+use crate::utils::config::Config;
+
 pub mod utils;
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let config = Config::new("./config/config.yaml")?;
+    let db_opt = C
+    
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use pesona_be::utils::config::{self, Config, ConfigError};
+
+fn main() -> Result<(), ConfigError> {
     println!("Hello, world!");
+    let config = Config::new("./config/config.yaml")?;
+    let port: String = config.get("jwt.audience")?;
+    println!("{}", port);
+
+    let url = config.get::<String>("database.url")?;
+    println!("{}", url);
+
+    Ok(())
 }
